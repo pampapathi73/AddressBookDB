30bbd7fccbf9ce64e915528b84b8af02e3a4b042
pampapathi73 <pampapathik32@gmail.com>
2021-07-02 15:22:29 +0530

    [pampapathi] Added: InsertValues

---
+++ UseCase3.sql
@@ -0,0 +1,26 @@
+create database Address_Book_ServiceDB
+select * from sys.databases;
+use Address_Book_ServiceDB
+
+///usecase2
+
+create table AddressBookDB
+(
+firstname varchar(20) not null,
+lastname varchar(20) not null,
+address varchar(20) not null,
+city varchar(20) not null,
+state varchar(20) not null,
+zip int not null,
+phoneNumber varchar(12) not null,
+email varchar(20)	not null,
+);
+
+/// usecase3
+
+select * from AddressBookDB 
+
+insert into AddressBookDB values
+('Ram','Sharma','House no 34','Tinsukia','Assam',786675,'8745923061','ramsharma@gmail.com'),
+('Niha','Das','House no 12','Namrup','Assam',786623,'9934567234','nihadas@gmail.com'),
+('Prakash','Dev','house no 9','Itanagar','Arunachal pradesh',873456,'8795678657','prakash@yahoo.com');
